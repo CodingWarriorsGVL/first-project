f72636f612c5f2ccf399957418f88a02eb4e514e
'Arjun <arjunkaycee@outlook.com>
2019-10-25 13:16:08 -0400

    My first set of changes

---
+++ Minesweeper/src/module-info.java
@@ -0,0 +1,12 @@
+module application {
+
+	requires javafx.base; 
+	requires javafx.controls;
+	requires javafx.fxml; 
+	requires javafx.graphics;
+	requires javafx.media; 
+	requires javafx.swing; 
+	requires javafx.swt; 
+	requires javafx.web; 
+	exports application; 
+}
